2bd566bc168f36de3019690f47547256bb20617f
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-12-27 20:38:10 -0700

    made temporary pages for other links

---
+++ career.php
@@ -0,0 +1,47 @@
+<!-- copilot prompt - html code for a fancy, over the top, way of saying "coming soon" -->
+<?php $thisPage="index"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coming Soon</title>
+    <style>
+        body {
+            background-color: #000;
+            color: #fff;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 200px;
+        }
+        
+        h1 {
+            font-size: 48px;
+            margin-bottom: 20px;
+        }
+        
+        p {
+            font-size: 24px;
+            margin-bottom: 40px;
+        }
+        
+        .glow {
+            animation: glowing 2s infinite;
+        }
+        
+        @keyframes glowing {
+            0% {
+                text-shadow: 0 0 10px #fff;
+            }
+            50% {
+                text-shadow: 0 0 20px #fff;
+            }
+            100% {
+                text-shadow: 0 0 10px #fff;
+            }
+        }
+    </style>
+</head>
+<body>
+    <h1 class="glow">Coming Soon</h1>
+    <p>Working on something amazing!</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  -->
 
  <?php $thisPage="index"; ?>
- 
+
  <!DOCTYPE html>
 <html>
 <head>
@@ -35,19 +35,19 @@
                     <li><a href="/"<?php if (isset($thisPage) && $thisPage=="index"){
                         echo " id=\"current-page\"";
                     }?>>Home</a></li>
-                    <li><a href="./career.php"<?php if (isset($thisPage) && $thisPage=="about"){
+                    <li><a href="/career.php"<?php if (isset($thisPage) && $thisPage=="about"){
                         echo " id=\"current-page\"";
                     }?>>Career</a></li>
                     <!-- <li><a href="./#contact-section"<?php #if (isset($thisPage) && $thisPage=="contact"){
                         # echo " id=\"current-page\"";
                     #}?>>contact</a></li> -->
-                    <li class="dropdown"><a href="digitial-portfolio.php" class="dropbtn" <?php if (isset($thisPage) && $thisPage=="user"){
+                    <li class="dropdown"><a href="/digitial-portfolio.php" class="dropbtn" <?php if (isset($thisPage) && $thisPage=="user"){
                         echo " id=\"current-page\"";
                     }?>>Digitial Portfolio<svg class="dvmRw" viewBox="0 0 24 24" stroke="currentColor" jsname="HIH2V" focusable="false"><g transform="translate(9.7,12) rotate(45)"><path class="K4B8Y" d="M-4.2 0 L4.2 0" stroke-width="2"></path></g><g transform="translate(14.3,12) rotate(-45)"><path class="MrYMx" d="M-4.2 0 L4.2 0" stroke-width="2"></path></g></svg>
                         <div class="dropdown-content">
-                            <a href="#" class="dropdown-links">Link 1</a>
-                            <a href="#" class="dropdown-links">Link 2</a>
-                            <a href="#" class="dropdown-links">Link 3</a>
+                            <a href="/coming-soon.php" class="dropdown-links">Link 1</a>
+                            <a href="/coming-soon.php" class="dropdown-links">Link 2</a>
+                            <a href="/coming-soon.php" class="dropdown-links">Link 3</a>
                         </div>
                     </a></li>
                 </ul>
