d70119d5c763948173c1f40dd3f814f26a3f84d7
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-12-27 19:49:33 -0700

    minor changes

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ Create me a personal website that has the current features:
             </label>
 
             <div class="header_content">
-                <a href="PersonalWebsite2.php" id="website-name">Tyler Bowes</a>
+                <a href="PersonalWebsite2.php" id="website-name">Hey, I'm Tyler Bowes</a>
                 <ul>
                     <li><a href="PersonalWebsite2.php"<?php if (isset($thisPage) && $thisPage=="index"){
                         echo " id=\"current-page\"";
