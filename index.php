5512892420ee94eee80f63f3fc42b97c98869c59
Tyler Bowes <tylerbowes@u.boisestate.edu>
2023-12-27 18:55:17 -0700

    added header from developed on localhost

--- index.php
+++ index.php
@@ -27,6 +27,41 @@ Create me a personal website that has the current features:
 
 </head>
 <body>
+    <header>
+        <nav class="site_header">            
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+                <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z">
+                    </path>
+                </svg>
+            </label>
+
+            <div class="header_content">
+                <a href="PersonalWebsite2.php" id="website-name">Tyler Bowes</a>
+                <ul>
+                    <li><a href="PersonalWebsite2.php"<?php if (isset($thisPage) && $thisPage=="index"){
+                        echo " id=\"current-page\"";
+                    }?>>Home</a></li>
+                    <li><a href="./career.php"<?php if (isset($thisPage) && $thisPage=="about"){
+                        echo " id=\"current-page\"";
+                    }?>>Career</a></li>
+                    <!-- <li><a href="./#contact-section"<?php #if (isset($thisPage) && $thisPage=="contact"){
+                        # echo " id=\"current-page\"";
+                    #}?>>contact</a></li> -->
+                    <li class="dropdown"><a href="digitial-portfolio.php" class="dropbtn" <?php if (isset($thisPage) && $thisPage=="user"){
+                        echo " id=\"current-page\"";
+                    }?>>Digitial Portfolio<svg class="dvmRw" viewBox="0 0 24 24" stroke="currentColor" jsname="HIH2V" focusable="false"><g transform="translate(9.7,12) rotate(45)"><path class="K4B8Y" d="M-4.2 0 L4.2 0" stroke-width="2"></path></g><g transform="translate(14.3,12) rotate(-45)"><path class="MrYMx" d="M-4.2 0 L4.2 0" stroke-width="2"></path></g></svg>
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-links">Link 1</a>
+                            <a href="#" class="dropdown-links">Link 2</a>
+                            <a href="#" class="dropdown-links">Link 3</a>
+                        </div>
+                    </a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
     <div class="container">
         <div class="profile-image">
             <img src="Images/Portrate (linkedin).jpg" alt="Profile Picture">
