48bfc223dd24c419652d2cbe81cc1fda23b888d7
Tyler Bowes <tylerbowes@u.boisestate.edu>
2024-01-01 17:11:48 -0700

    added images

--- digitial-portfolio.php
+++ digitial-portfolio.php
@@ -3,6 +3,10 @@
 <html>
 <head>
     <title>Digital Portfolio</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/Favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/Favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/Favicon/favicon-16x16.png">
+    <link rel="manifest" href="/Favicon/site.webmanifest">
     <link rel="stylesheet" href="/Styles/personalwebsite_styles.css">
 
 </head>
@@ -13,21 +17,21 @@
 
     <section>
         <h2>Section 1</h2>
-        <img src="images/section1.jpg" alt="Section 1 Image">
+        <img src="Images\MTF example photo.png" alt="Section 1 Image">
         <p>This is the first section of my portfolio.</p>
         <a href="Pages\MTF_documentation.php">MTF Tool</a>
     </section>
 
     <section>
         <h2>Section 2</h2>
-        <img src="images/section2.jpg" alt="Section 2 Image">
+        <img src="Images\macbeth_colorcheck_chart.jpg" alt="Section 2 Image">
         <p>This is the second section of my portfolio.</p>
         <a href="Pages\Color-checker.php">Color Fidelity Tool</a>
     </section>
 
     <section>
         <h2>Section 3</h2>
-        <img src="images/section3.jpg" alt="Section 3 Image">
+        <img src="Images\Swingreach forklift.jpg" alt="Section 3 Image">
         <p>This is the third section of my portfolio.</p>
         <a href="Pages\Curtis_projects.php">PDO and Solenoids</a>
     </section>
