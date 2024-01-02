7770ab766634520523bbf4205ef45bd43491c7af
Tyler Bowes <tylerbowes@u.boisestate.edu>
2024-01-02 15:58:11 -0700

    more styles

--- digitial-portfolio.php
+++ digitial-portfolio.php
@@ -20,6 +20,7 @@
             font-size: 20px;
             font-weight: bold;
             margin: 0px;
+            text-align: center;
         }
 
         .description {
@@ -47,24 +48,18 @@
             margin: 0 3%;
             display: inline-block;
             vertical-align: top;
-            background-color: #f2f2f2; /* Add your desired background color here */
-            padding: 20px; /* Add padding for better visual appearance */
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); /* Add box shadow */
+            background-color: #f0f0f0; 
+            padding: 20px; 
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
         }
+        section:hover {
+            transform: scale(1.2);  
+        }   
 
-        #image-1{
-            display: block;
+        .progect_images{
+            width: 100%;
+            height: auto;
             text-align: center;
-            margin-right: 10px;
-            margin-bottom: 10px;
-            width: 40%;
-        }
-
-        .figure-label #image-2{
-            display: block;
-            text-align: center;
-            margin-bottom: 10px;
-            width: 40%;
         }
 
     </style>
@@ -75,25 +70,25 @@
     <h1 class="title">My Digital Portfolio</h1>
 
     <section>
-        <h2>Section 1</h2>
+        <h2 class="headers">Section 1</h2>
         <a href="Pages\MTF_documentation.php" class="digital_portfolio_link">
-            <img src="Images\MTF example photo.png" alt="Section 1 Image" id="image-2">
+            <img src="Images\MTF example photo.png" alt="Section 1 Image" class="progect_images">
             <p class="discription">MTF Tool</p>
         </a>
     </section>
 
     <section>
-        <h2>Section 2</h2>
+        <h2 class="headers">Section 2</h2>
         <a href="Pages\Color-checker.php" class="digital_portfolio_link">
-            <img src="Images\macbeth_colorcheck_chart.jpg" alt="Section 2 Image" id="image-2">
+            <img src="Images\macbeth_colorcheck_chart.jpg" alt="Section 2 Image" class="progect_images">
             <p class="discription">Color Fidelity Tool</p>
         </a>
     </section>
 
     <section>
-        <h2>Section 3</h2>
+        <h2 class="headers">Section 3</h2>
         <a href="Pages\Curtis_projects.php" class="digital_portfolio_link">
-            <img src="Images\Swingreach forklift.jpg" alt="Section 3 Image" id="image-2">
+            <img src="Images\Swingreach forklift.jpg" alt="Section 3 Image" class="progect_images">
             <p class="discription">PDO and Solenoids</p>
         </a>
     </section>
