3bc9dada6b648b4d1d8d915cce0d7321a16ce774
Tyler Bowes <tylerbowes@u.boisestate.edu>
2024-01-02 16:38:46 -0700

    reverted change on section and updated pdf file

--- digitial-portfolio.php
+++ digitial-portfolio.php
@@ -82,30 +82,28 @@
         <a href="PDFs\Tyler Bowes Resume 2024.pdf" target=”_blank”>RESUME</a>.  Enjoy!
     </p>
 
-    <a href="Pages\MTF_documentation.php" class="digital_portfolio_link">
-        <section>
+    <section>
         <h2 class="headers">MTF Tool</h2>
+        <a href="Pages\MTF_documentation.php" class="digital_portfolio_link">
             <img src="Images\MTF example photo.png" alt="Section 1 Image" class="progect_images">
             <!-- <p class="discription">MTF Tool</p> -->
-        
-        </section>
-    </a>
+        </a>
+    </section>
 
-    <a href="Pages\Color-checker.php" class="digital_portfolio_link">
-        <section>
+    <section>
         <h2 class="headers">Color Fidelity Tool</h2>
+        <a href="Pages\Color-checker.php" class="digital_portfolio_link">
             <img src="Images\macbeth_colorcheck_chart.jpg" alt="Section 2 Image" class="progect_images">
             <!-- <p class="discription">Color Fidelity Tool</p> -->
-        </section>
-    </a>
-    
-    <a href="Pages\Curtis_projects.php" class="digital_portfolio_link">
-        <section>
+        </a>
+    </section>
+
+    <section>
         <h2 class="headers">PDO and Solenoids</h2>
+        <a href="Pages\Curtis_projects.php" class="digital_portfolio_link">
             <img src="Images\Swingreach forklift.jpg" alt="Section 3 Image" class="progect_images">
             <!-- <p class="discription">PDO and Solenoids</p> -->
-        </section>
-    </a>
-    
+        </a>
+    </section>
 </body>
 </html>
